33b8b00bbe9a633d47bfb6851449f3d421a478c4
Andrei Basarab <andy.basarab@gmail.com>
2023-01-31 01:56:00 +0200

    Updated the code to use the 3 LEDs on the NUCLEO-F767ZI board

--- firmware/obc/cubesat-1-fw-obc/src/main.rs
+++ firmware/obc/cubesat-1-fw-obc/src/main.rs
@@ -26,18 +26,32 @@ fn main() -> ! {
 
     // Init GPIO Led
     let p = pac::Peripherals::take().unwrap();
-    let gpioi = p.GPIOI.split();
-    let mut led = gpioi.pi1.into_push_pull_output();
+    let gpiob = p.GPIOB.split();
+    let mut led_green = gpiob.pb0.into_push_pull_output();
+    let mut led_blue = gpiob.pb7.into_push_pull_output();
+    let mut led_red = gpiob.pb14.into_push_pull_output();
 
     loop {
         // Dummy variable incrementing
         counter += 1;
 
         for _ in 0..10_000 {
-            led.set_high();
+            led_green.set_high();
         }
         for _ in 0..10_000 {
-            led.set_low();
+            led_green.set_low();
+        }
+        for _ in 0..10_000 {
+            led_blue.set_high();
+        }
+        for _ in 0..10_000 {
+            led_blue.set_low();
+        }
+        for _ in 0..10_000 {
+            led_red.set_high();
+        }
+        for _ in 0..10_000 {
+            led_red.set_low();
         }
     }
 }
