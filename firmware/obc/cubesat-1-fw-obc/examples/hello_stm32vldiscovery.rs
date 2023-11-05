45f873c3a7b5891cc3903a787f013dcb832eb096
Andrei Basarab <andy.basarab@gmail.com>
2023-11-05 16:11:31 +0200

    Added exit for hello_stm32vldiscovery example when using QEMU

--- firmware/obc/cubesat-1-fw-obc/examples/hello_stm32vldiscovery.rs
+++ firmware/obc/cubesat-1-fw-obc/examples/hello_stm32vldiscovery.rs
@@ -7,13 +7,18 @@
 
 use panic_semihosting as _;
 
-use cortex_m_semihosting::hprintln;
+use cortex_m_semihosting::{debug, hprintln};
 use stm32f1xx_hal as _;
 
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!");
+    hprintln!("Hello, stm32vldiscovery!");
+
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    debug::exit(debug::EXIT_SUCCESS);
+
     loop {}
 }
