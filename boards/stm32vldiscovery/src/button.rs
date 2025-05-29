063cffab0313b7b800f84c1a7c8c147cc707425f
andreivoina <andreivoina20@gmail.com>
2025-05-29 22:47:09 +0300

    Allign stm32vldiscovery with the main project

--- boards/stm32vldiscovery/src/button.rs
+++ boards/stm32vldiscovery/src/button.rs
@@ -3,17 +3,22 @@ use stm32f1xx_hal::{
     gpio::{Edge, ExtiPin, Input, Pin, PullDown, HL},
     pac::EXTI,
 };
+use sys_time::prelude::*;
 
 pub struct ButtonParameters<'a> {
     pub pin: Pin<'A', 0>,
     pub edge: Edge,
+    // pub syscfg: &'a mut SYSCFG,
     pub exti: &'a mut EXTI,
     pub afio: &'a mut Parts,
     pub cr: &'a mut <Pin<'A', 0> as HL>::Cr,
+    pub debounce_period: TimeDuration,
 }
 
 pub struct Button {
     btn: Pin<'A', 0, Input<PullDown>>,
+    debounce_period: TimeDuration,
+    pub debounce_instant: TimeInstant,
 }
 
 impl Button {
@@ -27,10 +32,22 @@ impl Button {
         button.trigger_on_edge(button_parameters.exti, button_parameters.edge);
         button.enable_interrupt(button_parameters.exti);
 
-        Self { btn: button }
+        Self {
+            btn: button,
+            debounce_period: button_parameters.debounce_period,
+            debounce_instant: SysTime::now(),
+        }
     }
 
     pub fn clear_interrupt_pending_bit(&mut self) {
         self.btn.clear_interrupt_pending_bit();
+    }
+
+    pub fn check_interrupt(&mut self) -> bool {
+        self.btn.check_interrupt()
+    }
+
+    pub fn get_debounce_period(&mut self) -> TimeDuration {
+        self.debounce_period
     }
 }
---
+++ boards/stm32vldiscovery/src/event_pin.rs
@@ -0,0 +1,40 @@
+use stm32f1xx_hal::{
+    gpio::{Edge, ExtiPin, Floating, Input, Pin},
+    pac::{AFIO, EXTI},
+    rcc::APB2,
+};
+
+pub struct EventPinParameters<'a, const P: char, const N: u8> {
+    pub pin: Pin<P, N, Input<Floating>>,
+    pub edge: Edge,
+    pub afio: &'a mut stm32f1xx_hal::afio::Parts,
+    pub exti: &'a mut EXTI,
+    pub apb: &'a mut APB2,
+}
+
+pub struct EventPin<const P: char, const N: u8> {
+    pin: Pin<P, N, Input<Floating>>,
+}
+
+impl<const P: char, const N: u8> EventPin<P, N> {
+    pub fn new(event_pin_parameters: EventPinParameters<P, N>) -> Self {
+        // let mut pin = event_pin_parameters.pin.into_floating_input();
+        let mut pin = event_pin_parameters.pin;
+        // Enable external interrupt on the pin
+        pin.make_interrupt_source(event_pin_parameters.afio);
+        pin.trigger_on_edge(event_pin_parameters.exti, event_pin_parameters.edge);
+        pin.enable_interrupt(event_pin_parameters.exti);
+
+        Self { pin }
+    }
+
+    pub fn clear_interrupt_pending_bit(&mut self) {
+        self.pin.clear_interrupt_pending_bit();
+    }
+
+    pub fn check_interrupt(&mut self) -> bool {
+        self.pin.check_interrupt()
+    }
+}
+
+pub type EventPinCc1101Gdo2 = EventPin<'D', 2>;
